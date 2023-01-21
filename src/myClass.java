9250db0766ecdb83316a07ad706b0a1207209649
arshaluis <amkrtchian91@gmail.com>
2023-01-21 20:27:12 +0200

    Homeworks

---
+++ src/myClass.java
@@ -0,0 +1,13 @@
+public class myClass {
+    int Year = 2023;
+    static int sumOfTwoNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String args[])
+    {
+        int a = 60, b = 60 , sum;
+        sum = a + b;
+        System.out.println("The sum of two numbers is: "+sum);
+    }
+}
